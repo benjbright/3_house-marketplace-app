e70166d2ca8e1f9dfdbefe5b0c01052a84345dca
Ben <105577567+benjbright@users.noreply.github.com>
2022-06-15 15:59:42 +0100

    Project work 15/06

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "react-toastify/dist/ReactToastify.css"
 import Navbar from "./components/Navbar"
 import PrivateRoute from "./components/PrivateRoute"
 import Explore from "./pages/Explore"
-import ForgotPassword from "./pages/ForgotPassword"
 import Offers from "./pages/Offers"
+import Category from "./pages/Category"
 import Profile from "./pages/Profile"
 import Signin from "./pages/Signin"
 import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+import CreateListing from "./pages/CreateListing"
 
 function App() {
   return (
@@ -17,12 +19,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
