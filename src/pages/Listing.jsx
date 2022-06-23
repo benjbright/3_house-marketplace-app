a398118f8c2715b081767ca7a01901d13d80e2a3
Ben <105577567+benjbright@users.noreply.github.com>
2022-06-23 09:10:30 +0100

    Project work 23/06
    Work on adding map and slider functionality to listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
+import "swiper/css/a11y"
 import { getDoc, doc } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 import { db } from "../firebase.config"
 import Spinner from "../components/Spinner"
 import shareIcon from "../assets/svg/shareIcon.svg"
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 const Listing = () => {
   const [listing, setListing] = useState(null)
@@ -21,7 +30,7 @@ const Listing = () => {
       const docSnap = await getDoc(docRef)
 
       if (docSnap.exists()) {
-        console.log(docSnap.data())
+        // console.log(docSnap.data())
         setListing(docSnap.data())
         setLoading(false)
       }
@@ -36,7 +45,20 @@ const Listing = () => {
 
   return (
     <main>
-      {/* SLIDER */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+                height: "50vh",
+              }}
+              className="swiperSlideDiv"
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div
         className="shareIconDiv"
@@ -91,7 +113,25 @@ const Listing = () => {
 
         <p className="listingLocationTitle">Location</p>
 
-        {/* MAP */}
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: "100%", width: "100%" }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+            />
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
@@ -106,3 +146,5 @@ const Listing = () => {
   )
 }
 export default Listing
+
+// https://stackoverflow.com/questions/67552020/how-to-fix-error-failed-to-compile-node-modules-react-leaflet-core-esm-pat
