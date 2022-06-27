e01dadc3fa169b65cb0a6a5d199ef1d25b557765
Ben <105577567+benjbright@users.noreply.github.com>
2022-06-27 15:11:39 +0100

    Final project work

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -6,17 +6,18 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from "firebase/storage"
-import { addDoc, collection, serverTimestamp } from "firebase/firestore"
+import { doc, updateDoc, getDoc, serverTimestamp } from "firebase/firestore"
 import { db } from "../firebase.config"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { toast } from "react-toastify"
 import { v4 as uuidv4 } from "uuid"
 import Spinner from "../components/Spinner"
 
-const CreateListing = () => {
+const EditListing = () => {
   // eslint-disable-next-line
   const [geoLocationEnabled, setGeoLocationEnabled] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [listing, setListing] = useState(false)
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -53,8 +54,38 @@ const CreateListing = () => {
 
   const auth = getAuth()
   const navigate = useNavigate()
+  const params = useParams()
   const isMounted = useRef(true)
 
+  //   Redirect if listing is not user's
+  useEffect(() => {
+    if (listing && listing.userRef !== auth.currentUser.uid) {
+      toast.error("You cannot edit that listing")
+      navigate("/")
+    }
+    // eslint-disable-next-line
+  }, [])
+
+  //   Fetch listing to edit
+  useEffect(() => {
+    setLoading(true)
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId)
+      const docSnap = await getDoc(docRef)
+      if (docSnap.exists()) {
+        setListing(docSnap.data())
+        setFormData({ ...docSnap.data(), address: docSnap.data().location })
+        setLoading(false)
+      } else {
+        navigate("/")
+        toast.error("Listing does not exist")
+      }
+    }
+
+    fetchListing()
+  }, [params.listingId, navigate])
+
+  //   Sets userRef to logged in user
   useEffect(() => {
     if (isMounted) {
       onAuthStateChanged(auth, (user) => {
@@ -190,8 +221,9 @@ const CreateListing = () => {
     // location && (formDataCopy.location = location)
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
-    const docRef = await addDoc(collection(db, "listings"), formDataCopy)
-
+    // Update listing
+    const docRef = doc(db, "listings", params.listingId)
+    await updateDoc(docRef, formDataCopy)
     setLoading(false)
     toast.success("Listing saved")
     navigate(`/category/${formDataCopy.type}/${docRef.id}`)
@@ -232,7 +264,7 @@ const CreateListing = () => {
   return (
     <div className="profile">
       <header>
-        <p className="pageHeader">Create a Listing</p>
+        <p className="pageHeader">Edit Listing</p>
       </header>
 
       <main>
@@ -459,11 +491,11 @@ const CreateListing = () => {
             required
           />
           <button type="submit" className="primaryButton createListingButton">
-            Create Listing
+            Edit Listing
           </button>
         </form>
       </main>
     </div>
   )
 }
-export default CreateListing
+export default EditListing
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+// import { useParams } from "react-router-dom"
 import {
   collection,
   getDocs,
@@ -17,8 +17,9 @@ import ListingItem from "../components/ListingItem"
 const Offers = () => {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
-  const params = useParams()
+  // const params = useParams()
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -36,6 +37,9 @@ const Offers = () => {
 
         // Execute query
         const querySnap = await getDocs(q)
+
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+        setLastFetchedListing(lastVisible)
 
         const listings = []
 
@@ -55,6 +59,43 @@ const Offers = () => {
 
     fetchListings()
   }, [])
+
+  // Pagination / load more
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, "listings")
+
+      // Create a query
+      const q = query(
+        listingsRef,
+        where("offer", "==", true),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(10)
+      )
+
+      // Execute query
+      const querySnap = await getDocs(q)
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+      setLastFetchedListing(lastVisible)
+
+      const listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setListings((prevState) => [...prevState, ...listings])
+      setLoading(false)
+    } catch (error) {
+      toast.error("Could not fetch listings")
+    }
+  }
 
   return (
     <div className="category">
@@ -77,6 +118,14 @@ const Offers = () => {
               ))}
             </ul>
           </main>
+
+          <br />
+          <br />
+          {lastFetchedListing && (
+            <p className="loadMore" onClick={onFetchMoreListings}>
+              Load More
+            </p>
+          )}
         </>
       ) : (
         <p>There are no current offers</p>
